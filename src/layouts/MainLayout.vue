4ba2300d0db20df3a18b268072976167837310cc
Juanma Cano <juanmacano@gmail.com>
2021-07-11 18:31:06 +0200

    initial

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,77 @@
+<template>
+	<q-layout view="lHh Lpr lFf">
+		<q-header elevated>
+			<q-toolbar class="bblue">
+				<q-btn
+					flat
+					dense
+					round
+					icon="menu"
+					aria-label="Menu"
+					@click="toggleLeftDrawer"
+				/>
+
+				<q-toolbar-title> ToDo Windows </q-toolbar-title>
+
+				<div>juanmacano@gmail.com</div>
+			</q-toolbar>
+		</q-header>
+
+		<q-drawer
+			v-model="leftDrawerOpen"
+			show-if-above
+			bordered
+			class="bg-grey-1"
+		>
+			<q-list>
+				<q-item-label header class="text-grey-8">
+					Menú de navegación
+				</q-item-label>
+
+				<EssentialLink
+					v-for="link in essentialLinks"
+					:key="link.title"
+					v-bind="link"
+				/>
+			</q-list>
+		</q-drawer>
+
+		<q-page-container>
+			<router-view />
+		</q-page-container>
+	</q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+	{
+		title: "ToDo",
+		icon: "school",
+		link: "/",
+	},
+];
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+	name: "MainLayout",
+
+	components: {
+		EssentialLink,
+	},
+
+	setup() {
+		const leftDrawerOpen = ref(false);
+
+		return {
+			essentialLinks: linksList,
+			leftDrawerOpen,
+			toggleLeftDrawer() {
+				leftDrawerOpen.value = !leftDrawerOpen.value;
+			},
+		};
+	},
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,103 @@
+<template>
+	<div class="q-pa-md q-gutter-sm">
+		<q-editor
+			v-model="editor"
+			min-height="5rem"
+			:definitions="{
+				save: {
+					tip: 'Save your task',
+					icon: 'save',
+					label: 'Guardar',
+					handler: saveTask,
+				},
+			}"
+			:toolbar="[['bold', 'italic', 'strike', 'underline'], ['save']]"
+		/>
+
+		<q-card
+			flat
+			bordered
+			v-for="(item, index) in tasks"
+			:key="'task_' + index"
+			class="row"
+		>
+			<q-card-section
+				v-html="item.texto"
+				class="col"
+				:class="item.state ? 'tachar' : ''"
+			/>
+			<q-btn flat color="blue" @click="item.state = !item.state">
+				<span v-if="!item.state">Completed</span>
+				<span v-else>Pending</span>
+			</q-btn>
+			<q-btn flat color="red" @click="removeTask(index)"> Remove </q-btn>
+		</q-card>
+		<div v-if="!tasks.length">
+			<q-banner rounded class="bblue text-white"> No tasks. </q-banner>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+import App from "src/App.vue";
+
+export default {
+	/* 	components: { App }, */
+	setup() {
+		const $q = useQuasar();
+		const editor = ref("Next <b>task</b>");
+		let tasks = ref([
+			{
+				texto: "Task #1",
+				state: false,
+			},
+			{
+				texto: "Task #2",
+				state: true,
+			},
+			{
+				texto: "Task #3",
+				state: true,
+			},
+		]);
+
+		return {
+			editor,
+			tasks,
+			saveTask() {
+				tasks.value.push({
+					texto: editor.value,
+					state: false,
+				});
+				$q.notify({
+					type: "positive",
+					message: "Task saved !!",
+				});
+				editor.value = "";
+			},
+			removeTask(index) {
+				$q.dialog({
+					title: "Danger !!",
+					message: "Do you really want to remove the task?",
+					cancel: true,
+					persistent: true,
+				}).onOk(() => {
+					tasks.value.splice(index, 1);
+					$q.notify({
+						type: "negative",
+						message: "Task removed !!",
+					});
+				});
+			},
+		};
+	},
+};
+</script>
+
+<style scoped>
+.tachar {
+	text-decoration: line-through;
+}
+</style>
